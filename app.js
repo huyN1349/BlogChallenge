0efcede1ed73af9d89c7779c2c0691a9b0007b98
HuyN1349 <huy.nguyen1349@gmail.com>
2019-07-22 02:03:37 -0700

    Add mongoose, store posts in MongoDB, add Compost button in header.ejs

--- app.js
+++ app.js
@@ -1,27 +1,65 @@
 //jshint esversion:6
-
+//Declare Express dependency
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+//Declare Mongoose dependency
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+mongoose.connect("mongodb://localhost:27017/blogDB",{useNewUrlParser: true});
+const postSchema = new Schema({
+  title: String,
+  content: String,
+  datePosted: Date,
+})
+const Post = mongoose.model("Post",postSchema);
+
+const pageSchema = new Schema({
+  title: String,
+  content: String,
+  datePosted: Date,
+})
+
+const firstPost = new Post({
+  title: "Home",
+  content:"Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.",
+});
+//firstPost.save();
+
+const aboutPost = new Post({
+  title: "About",
+  content: "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.",
+});
+//aboutPost.save();
+
+const contactPost = new Post({
+  title:"Contact",
+  content:"Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.",
+});
+//contactPost.save();
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-const app = express();
-
 var posts = [];
 
-app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
 
 app.get("/", function(req,res){
-  res.render("home",{
-    homeStartingContentEJS: homeStartingContent,
-    postsEJS: posts,
-  });
+  Post.find({}, function(err, foundPost){
+    //console.log(foundPost[1]);
+    res.render("home",{
+      homeStartingContentEJS: firstPost,
+      postsEJS: foundPost,
+    });
+  })
 })
 
 app.get("/about", function(req,res){
@@ -31,6 +69,7 @@ app.get("/about", function(req,res){
 })
 
 app.get("/contact", function(req,res){
+
   res.render("contact", {
     contactContentEJS: contactContent,
   })
@@ -41,14 +80,22 @@ app.get("/compose", function(req,res){
 })
 
 app.post("/compose", function(req,res){
-  const post = {
-    title: req.body.blogTitle,
-    content: req.body.blogPost,
-  };
-  posts.push(post);
+  const postTitle = req.body.blogTitle;
+  const postContent = req.body.blogPost;
+  const date = new Date();
+  Post.find({title:postTitle}, function(err, foundPost){
+    if (foundPost.length > 0) {
+      console.log("Please try different title.")
+    } else {
+      Post.create({
+        title: postTitle,
+        content: postContent,
+        datePosted: date,
+      });
+      console.log("Successfully save new post.")
+    }
+  });
   res.redirect("/");
-  //console.log(posts);
-  //console.log(req.body.newBlog);
 })
 
 app.get("/posts/:topic", function(req,res) {
@@ -59,19 +106,11 @@ app.get("/posts/:topic", function(req,res) {
         postTitleEJS: posts[i].title,
         postContentEJS: posts[i].content,
       });
-      //console.log("Match Found!");
       break;
     } else {
-      //console.log("No Found");
     }
   }
-  //console.log(req.params.topic);
 })
-
-
-
-
-
 
 
 app.listen(process.env.PORT || 3000, function() {
